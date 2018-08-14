c91a7d4393310e6b329db963920e62e9d012a72d
Your Name <fahrisyany@gmail.com>
2018-08-14 09:20:30 +0700

    html done

---
+++ client/main.js
@@ -0,0 +1,68 @@
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+// navigation scroll lijepo radi materem
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+
+$(document).ready(function() {
+	
+	// INITIATE THE FOOTER
+  siteFooter();
+	// COULD BE SIMPLIFIED FOR THIS PEN BUT I WANT TO MAKE IT AS EASY TO PUT INTO YOUR SITE AS POSSIBLE
+	$(window).resize(function() {
+		siteFooter();
+	});
+	
+	function siteFooter() {
+		var siteContent = $('#site-content');
+		var siteContentHeight = siteContent.height();
+		var siteContentWidth = siteContent.width();
+
+		var siteFooter = $('#site-footer');
+		var siteFooterHeight = siteFooter.height();
+		var siteFooterWidth = siteFooter.width();
+
+		console.log('Content Height = ' + siteContentHeight + 'px');
+		console.log('Content Width = ' + siteContentWidth + 'px');
+		console.log('Footer Height = ' + siteFooterHeight + 'px');
+		console.log('Footer Width = ' + siteFooterWidth + 'px');
+
+		siteContent.css({
+			"margin-bottom" : siteFooterHeight + 50
+		});
+	};
+});
+
+
+

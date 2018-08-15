5cbfa58a1158a1981f3f1b5d22707e90067d0bcc
fajartc02 <fajartc02@gmail.com>
2018-08-15 17:08:02 +0700

    move components foods

--- client/js/vue.js
+++ client/js/vue.js
@@ -29,34 +29,35 @@ let app = new Vue({
     //   tag: '#laptop'
     // }
     ],
-    foods: [
-      {
-        id: 1,
-        name: 'bakso',
-        category: 'Food',
-        description: 'Bakso Malang mantab enaaaak! dijamin nyesel deh ngga nyobain',
-        price: 12000,
-        qty: 12,
-        img: 'https://anekaresepnusantara.info/wp-content/uploads/2017/03/Resep-Bakso-Urat-Sapi-Spesial-dan-Mantap.jpg'
-      },
-      {
-        id: 2,
-        name: 'Nasi Goreng',
-        category: 'Food',
-        description: 'Nasi Goreng Makjambrong, mantaaab!',
-        price: 15000,
-        qty: 10,
-        img: 'https://scm-assets.constant.co/scm/unilever/e9dc924f238fa6cc29465942875fe8f0/944501ee-4a97-47ee-864a-1d66bbe5bd29.jpg'
-      },
-      {
-        id: 3,
-        name: 'Bubur Ayam',
-        category: 'Food',
-        description: 'Bubur ayam cirebon yang di buat dengan bumbu ayam pilihan',
-        price: 10000,
-        qty: 13,
-        img: 'http://www.damniloveindonesia.com/image/catalog/explore_indonesia/Artikel/Taste/bubur/bubur_opening.jpg'
-      }],
+    // ================================= MOVE TO COMPONENT ===============================
+    // foods: [
+    //   {
+    //     id: 1,
+    //     name: 'bakso',
+    //     category: 'Food',
+    //     description: 'Bakso Malang mantab enaaaak! dijamin nyesel deh ngga nyobain',
+    //     price: 12000,
+    //     qty: 12,
+    //     img: 'https://anekaresepnusantara.info/wp-content/uploads/2017/03/Resep-Bakso-Urat-Sapi-Spesial-dan-Mantap.jpg'
+    //   },
+    //   {
+    //     id: 2,
+    //     name: 'Nasi Goreng',
+    //     category: 'Food',
+    //     description: 'Nasi Goreng Makjambrong, mantaaab!',
+    //     price: 15000,
+    //     qty: 10,
+    //     img: 'https://scm-assets.constant.co/scm/unilever/e9dc924f238fa6cc29465942875fe8f0/944501ee-4a97-47ee-864a-1d66bbe5bd29.jpg'
+    //   },
+    //   {
+    //     id: 3,
+    //     name: 'Bubur Ayam',
+    //     category: 'Food',
+    //     description: 'Bubur ayam cirebon yang di buat dengan bumbu ayam pilihan',
+    //     price: 10000,
+    //     qty: 13,
+    //     img: 'http://www.damniloveindonesia.com/image/catalog/explore_indonesia/Artikel/Taste/bubur/bubur_opening.jpg'
+    //   }],
     drinks: [
       {
         id: 4,
@@ -126,6 +127,7 @@ let app = new Vue({
       })
       return price
     },
+    // ================================= MOVE TO COMPONENT ===============================
     convertMoney: function (price) {
       return `Rp. ${price.toLocaleString()}`
     },
--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
+const cors = require('cors')
 
 
 var indexRouter = require('./routes/index');
@@ -20,6 +22,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', () => {
+  console.log("Connected dabatase success to Mlab")
+})
+
 app.use(cors())
 
 app.use('/', indexRouter);

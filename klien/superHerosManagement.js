c683011b07d68c7c9b4b44a12654eaba0b603667
rizariza69 <riza.riza69@gmail.com>
2018-08-20 05:04:52 +0700

    rz

---
+++ klien/superHerosManagement.js
@@ -0,0 +1,20 @@
+Vue.component('superHeros-management', {
+    templete: 
+    ` <a class="navbar-brand" href="#">
+        SuperHeroStore
+      </a>
+    `
+        ,
+        methods: {
+            
+            }
+
+        },
+        data(){
+            return {
+               
+            }
+        }
+        
+    
+})

c288832c2354ed8a0c139f9707c053df7cf92cf4
sumarsanaadi <sumarsana.adi@gmail.com>
2018-08-20 08:18:31 +0700

    remove firebase config from git

--- client-vue-directive/js/firebaserc.js
+++ client-vue-directive/js/firebaserc.js
@@ -1,10 +1,5 @@
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyD1UjvAxjyfenkBDqYMO2KpCMWxTkBbL74",
-  authDomain: "portfolio-556cd.firebaseapp.com",
-  databaseURL: "https://portfolio-556cd.firebaseio.com",
-  projectId: "portfolio-556cd",
-  storageBucket: "portfolio-556cd.appspot.com",
-  messagingSenderId: "930810400308"
+ 
 };
 firebase.initializeApp(config);
